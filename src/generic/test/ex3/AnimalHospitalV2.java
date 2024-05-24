0cf3e5f2bb56b5ec54cbb9d833d9988ae248a99a
ojg <jeongkeepscalm@gmail.com>
2024-05-24 14:51:57 +0900

    제네릭 타입 매개변수 제한

---
+++ src/generic/test/ex3/AnimalHospitalV2.java
@@ -0,0 +1,24 @@
+package generic.test.ex3;
+
+public class AnimalHospitalV2<T> {
+
+  private T animal;
+
+  public void set(T animal) {
+    this.animal = animal;
+  }
+
+  public void checkup() {
+    // 컴파일 오류: 클래스 내 해당 메소드 정의시 T 타입을 알 수 없다. Object 의 기능만 사용 가능  
+    // System.out.println("동물 이름: " + animal.getName());
+    // System.out.println("동물 크기: " + animal.getSize());
+    // animal.sound();
+  }
+
+  // 큰 동물을 반환
+  public T bigger(T target) {
+    // 컴파일 오류
+    // return animal.getSize() > target.getSize() ? animal : target;
+    return null;
+  }
+}
