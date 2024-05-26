694bd6ade3dd53c5ae38075119cc94ef01177504
ojg <jeongkeepscalm@gmail.com>
2024-05-26 16:45:20 +0900

    generic method

---
+++ src/generic/test/ex4/MethodMain1.java
@@ -0,0 +1,20 @@
+package generic.test.ex4;
+
+public class MethodMain1 {
+  public static void main(String[] args) {
+    Integer i = 10;
+    Object o = GenericMethod.objMethod(1);
+
+    // Generic Method
+    Integer i1 = GenericMethod.<Integer>genericMethod(i);
+    Integer i2 = GenericMethod.<Integer>numberMethod(30);
+    Double d = GenericMethod.<Double>numberMethod(3.6);
+
+    // 컴파일러가 타입추론하여 생략 가능
+    Integer i3 = GenericMethod.genericMethod(i);
+    Integer i4 = GenericMethod.numberMethod(30);
+    Double d1 = GenericMethod.numberMethod(3.6);
+
+  }
+
+}
---
+++ src/generic/test/ex4/MethodMain2.java
@@ -0,0 +1,28 @@
+package generic.test.ex4;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain2 {
+
+  public static void main(String[] args) {
+
+    Dog dog = new Dog("dog1", 100);
+    Cat cat = new Cat("cat1", 200);
+
+    /**
+     * 타입 매개변수 추론으로 생략 가능
+     */
+    AnimalMethod.checkup(dog);
+    AnimalMethod.checkup(cat);
+    // AnimalMethod.<Dog>checkup(dog);
+    // AnimalMethod.<Cat>checkup(cat);
+
+    Dog targetDog = new Dog("targetDog", 150);
+    Dog biggerOne = AnimalMethod.bigger(targetDog, dog);
+    // Dog biggerOne = AnimalMethod.<Dog>bigger(targetDog, dog);
+    System.out.println("biggerOne = " + biggerOne);
+
+  }
+
+}
